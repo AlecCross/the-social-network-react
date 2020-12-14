84b41d81573c19c43456e8c85cfbb897dbc3fb8b
AlecCross <cross2472@gmail.com>
2020-12-14 05:59:36 +0200

    Add & test routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import s from './Navbar.module.css';
 let Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div><a className={s.a}>Profile</a></div>
-            <div><a className={s.a}>Messages</a></div>
+            <div><a href="/profile" className={s.a}>Profile</a></div>
+            <div><a  href="/dialogs"  className={s.a}>Messages</a></div>
             <div><a className={s.a}>News</a></div>
             <div><a className={s.a}>Music</a></div>
             <div><a className={s.a}>Settings</a></div>
