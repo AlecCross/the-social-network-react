4d0e6ee45e408fb24ad1729fbef00cdaa5a71b3c
AlecCross <cross2472@gmail.com>
2020-12-29 03:46:37 +0200

    refactoring: create state.js obj for data 29.1

--- src/index.js
+++ src/index.js
@@ -6,33 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import Dialog from './components/dialogs/dialogItem/Dialog';
 import Message from './components/dialogs/message/Message';
 import Post from './components/profile/post/Post';
-
-let dialogs = [
-  { id: 1, name: 'Andrew' },
-  { id: 2, name: 'Sasha' },
-  { id: 3, name: 'Vova' },
-  { id: 4, name: 'Ira' },
-  { id: 5, name: 'Vika' },
-  { id: 6, name: 'Dasha' },
-]
-
-let messages = [
-  { id: 1, message: 'Привет!' },
-  { id: 2, message: 'Доров' },
-  { id: 3, message: 'Как там с поиском работы?' },
-]
-
-let stockAvatar = 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png';
-let posts = [
-  { id: 1, text: 'Пост1', avatar: stockAvatar },
-  { id: 2, text: 'Пост2', avatar: stockAvatar },
-  { id: 3, text: 'Пост3', avatar: stockAvatar },
-  { id: 4, text: 'Пост4', avatar: stockAvatar },
-]
+import state from './redux/state';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App dialogs={dialogs} messages={messages} posts = {posts}/>
+    <App dialogs={state.dialogs} messages={state.messages} posts = {state.posts}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    dialogs: [
+        { id: 1, name: 'Andrew' },
+        { id: 2, name: 'Sasha' },
+        { id: 3, name: 'Vova' },
+        { id: 4, name: 'Ira' },
+        { id: 5, name: 'Vika' },
+        { id: 6, name: 'Dasha' },
+    ],
+    messages: [
+        { id: 1, message: 'Привет!' },
+        { id: 2, message: 'Доров' },
+        { id: 3, message: 'Как там с поиском работы?' },
+    ],
+    //stockAvatar: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png',
+    posts: [
+        { id: 1, text: 'Пост1', avatar: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png' },
+        { id: 2, text: 'Пост2', avatar: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png' },
+        { id: 3, text: 'Пост3', avatar: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png' },
+        { id: 4, text: 'Пост4', avatar: 'https://lumpics.ru/wp-content/uploads/2017/11/Programmyi-dlya-sozdaniya-avatarok.png' },
+    ],
+}
+
+export default state
