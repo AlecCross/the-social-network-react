adb6c8d0e1c1ef4e8eb2fb39f340757ff2e4b1bb
AlecCross <cross2472@gmail.com>
2020-12-31 05:10:08 +0200

    39.1 An attempt to understand how a actionCreator works

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -2,18 +2,30 @@
 import React from 'react';
 import Post from '../post/Post';
 
-let newPostElement = React.createRef();
+let addPostActionCreator = () => {
+    return {
+        type: 'ADD-POST',
+    }
+}
+let updateNewPostTextActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT',
+        newText: text,
+    }
+}
 
 const Posts = (props) => {
     let postsElements = props.posts.map(p => <Post text={p.text} avatar={p.avatar} id={p.id} />);
 
+    let newPostElement = React.createRef();
+
     let addPost = () => {
         // props.addPost();
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     }
     let onPostChange = () =>{
-        // let text = newPostElement.current.value;
-        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: newPostElement.current.value};
+        let text = newPostElement.current.value;
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
     }
     return (
